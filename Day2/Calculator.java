7d756b6d42c683da727990e7682859f02a8920ad
Nagu-automation <117554576+Nagu-automation@users.noreply.github.com>
2022-11-08 18:58:14 +0530

    Add files via upload
    Adding Day 2 assignments

---
+++ Day2/Calculator.java
@@ -0,0 +1,25 @@
+package week1.Day2;
+
+public class Calculator {
+
+	public int add(int num1, int num2, int num3) {
+		int sum = num1 + num2 + num3;
+		return sum;
+	}
+
+	public int sub(int num1, int num2) {
+		int subt = num1 + num2;
+		return subt;
+	}
+
+	public double mul(double num1, double num2) {
+		double mult = num1 * num2;
+		return mult;
+	}
+
+	public float div(float num1, float num2) {
+		float divi = num1 / num2;
+		return divi;
+	}
+
+}
