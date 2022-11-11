4466df2dd586fdcb9038eab73488b73264c47f74
Nagu-automation <117554576+Nagu-automation@users.noreply.github.com>
2022-11-11 19:50:33 +0530

    Update CheckIfTheNumberIsPositiveOrNegative.java
    Updating the comment line for each statement

--- week1/Assgn/CheckIfTheNumberIsPositiveOrNegative.java
+++ week1/Assgn/CheckIfTheNumberIsPositiveOrNegative.java
@@ -4,19 +4,19 @@ public class CheckIfTheNumberIsPositiveOrNegative {
 
 	public static void main(String[] args) {
 		
-		int number = 0;
+		int number = 0; //initializing and declaring the value
 		
-		if(number > 0)
+		if(number > 0) //Checking that the given number is greater than 0
 		{
-			System.out.println("The number " + number + " is postive number");
+			System.out.println("The number " + number + " is postive number"); //Printing the statement as positive if the given value is greater than zero
 		}
-		else if(number < 0)
+		else if(number < 0) //Checking that the given number is greater than 0
 		{
-			System.out.println("The number " + number + " is negative number");
+			System.out.println("The number " + number + " is negative number"); //Printing the statement as negative if the given value is less than zero
 		}
 		else
 		{
-			System.out.println("The number " + number + " is neither postive nor negative number");
+			System.out.println("The number " + number + " is neither postive nor negative number"); //Printing the statement if the given value is zero
 		}
 
 	}
