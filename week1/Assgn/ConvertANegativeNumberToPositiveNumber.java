622a8b1d41ce7ec79d659a1af0f945e693d3935d
Nagu-automation <117554576+Nagu-automation@users.noreply.github.com>
2022-11-11 19:55:08 +0530

    Update ConvertANegativeNumberToPositiveNumber.java
    Updated comment line for each code

--- week1/Assgn/ConvertANegativeNumberToPositiveNumber.java
+++ week1/Assgn/ConvertANegativeNumberToPositiveNumber.java
@@ -4,15 +4,15 @@ public class ConvertANegativeNumberToPositiveNumber {
 
 	public static void main(String[] args) {
 		
-		int value = -10;
+		int value = -10; //Initializing and declaring the value
 		
-		if(value < 0)
+		if(value < 0) //Checking the given value is less than zero
 		{
-			int conValue = value * -1;
-			System.out.println("The give number " + value + " is converted to positive number " + conValue);
+			int conValue = value * -1; //If the condition satisfies, then the given value is multiplied with -1 to convert the negative value to positive value
+			System.out.println("The give number " + value + " is converted to positive number " + conValue); //Printing the coverted value
 		}else
 		{
-			System.out.println("The give number " + value + " is already positive number");
+			System.out.println("The give number " + value + " is already positive number"); // The statement will print if the given value is a positive value
 		}
 
 	}
