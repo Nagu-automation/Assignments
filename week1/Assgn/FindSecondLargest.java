64ceacb3119a4549945b7d7908dc7e4ed92956a1
Nagu-automation <117554576+Nagu-automation@users.noreply.github.com>
2022-11-11 20:16:11 +0530

    Update FindSecondLargest.java
    Updating the comment for each line of code

--- week1/Assgn/FindSecondLargest.java
+++ week1/Assgn/FindSecondLargest.java
@@ -6,11 +6,11 @@ public class FindSecondLargest {
 
 	public static void main(String[] args) {
 		
-		int value[] = {3,2,11,4,6,7};
+		int value[] = {3,2,11,4,6,7}; //Intializing and declaring the array value
 		
-		int[] valueSort = ArraySorter.sort(value);
+		int[] valueSort = ArraySorter.sort(value); //Sorting the array value and assigning that to a new variable
 		
-		System.out.println("The Second largest value is: " + valueSort[value.length-2]);
+		System.out.println("The Second largest value is: " + valueSort[value.length-2]); //Printing the second largest value with the help of subtracting the total array length -2 to get the right index value
 
 	}
 
